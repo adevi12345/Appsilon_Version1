2633533bbb58498aec1934e1f74eb7844e12b0dd
adevi12345 <92974092+adevi12345@users.noreply.github.com>
2022-01-16 00:18:09 +0530

    Updated latest one

--- app.R
+++ app.R
@@ -36,13 +36,8 @@ ui <- dashboardPage(
 server <- function(input, output,session) {
       values <- reactiveValues()
     
-    values$input_dataset<-read_xlsx("D:/FL/Appsilon/Shiny_Developer/dummy_input.xlsx")
-    observe({
-        
-        if( is.null(values$map_table )){
-            return("Hi")
-        }
-    })
+    values$input_dataset<-read_xlsx("./dummy_input.xlsx")
+    
      observeEvent(input$searchbutton,{
         
         print(input$search_name)
@@ -50,26 +45,12 @@ server <- function(input, output,session) {
         values$checkbox_species<-filter(values$input_dataset,`Scientific name` == input$search_name)
 
         output$checkbox_ui<-renderUI({
-            checkboxInput("all_none","select ALL/None",value = TRUE)
-
+           
             checkboxGroupInput("checkbox_speciesgroup","select one",choices =  values$checkbox_species$Species)
             
         })
        
     })
-    
-     
-     
-      # observe({
-
-
-          
-         
-         # updateCheckboxGroupInput(
-     #     session,'checkbox_species',choices = values$checkbox_species$Species,selected = if(input$all_none) values$checkbox_species$Species
-     # )
-     #     
-      # })
     observeEvent(input$checkbox_speciesgroup,{
         
         values$checkbox_values<-input$checkbox_speciesgroup
